a531d39a46e008fb8c8f59f8512a61ae5c8fe63f
cedMac <c.mace@webandcow.com>
2022-11-26 18:06:36 +0100

    Modification de l'app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <the-header></the-header>
+    <div class="d-flex flex-row">
+      <the-shop class="w-75"></the-shop>
+      <the-cart  class="w-25"></the-cart>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from "./components/TheHeader.vue";
+import TheShop from "./components/Shop/TheShop.vue";
+import TheCart from "./components/Cart/TheCart.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TheHeader,
+    TheShop,
+    TheCart,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>

0531548ec37dcc2d571ed504415e6145345645bb
cedMac <c.mace@webandcow.com>
2022-12-06 14:28:13 +0100

    Dev item product

--- src/components/Shop/ShopProductItem.vue
+++ src/components/Shop/ShopProductItem.vue
@@ -1,19 +1,43 @@
 <template>
-    <div class="w-25 border product-container">
-        Item
+  <div class="w-25 p-3 product-container d-flex flex-column">
+    <div class="d-flex flex-row justify-content-center">
+        <img style="height:150px" :src="product.img" :alt="product.title">
     </div>
+    <div>
+        <h3> {{product.title}}</h3>
+        <hr>
+    </div>
+    <div>
+        <p>{{ product.description}}</p>
+    </div>
+    <div class="d-flex flex-row justify-content-between">
+        <span>{{ product.price}} €</span>
+        <button @click="addProductToCart" class="btn btn-primary btn-sm"> Commander </button>
+
+    </div>
+  </div>
 </template>
 
 <script>
+import { eventBus } from '../../main'
 
 export default {
+  props: ['product'],
+  methods:{
+    addProductToCart(){
+      eventBus.addProductToCart({...this.product})
 
-}
+    }
+  }
+};
 </script>
 
 <style>
-.product-container{
-    height: 200px;
-    padding: 5px
+.product-container {
+  height: 400px;
+  padding: 5px;
+}
+p{
+    font-size : 12px
 }
 </style>

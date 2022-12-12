508032e9c77f650967e99d7e3160b5b7eebdec96
cedMac <c.mace@webandcow.com>
2022-12-12 16:47:36 +0100

    Mise en place de la nouvelle architecture

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
     <the-header></the-header>
-    <div class="d-flex flex-row">
-      <the-shop class="w-75"></the-shop>
-      <the-cart  class="w-25"></the-cart>
-    </div>
+    <the-user></the-user>
+    <the-admin></the-admin>
+
   </div>
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader.vue";
-import TheShop from "./components/Shop/TheShop.vue";
-import TheCart from "./components/Cart/TheCart.vue";
+import TheHeader from "./components/TheHeader";
+import TheUser from "./components/features/User/TheUser";
+import TheAdmin from "./components/features/Admin/TheAdmin";
+
 
 export default {
   name: "App",
   components: {
     TheHeader,
-    TheShop,
-    TheCart,
+    TheUser,
+    TheAdmin
   },
 };
 </script>
---
+++ src/components/features/User/TheUser.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex flex-row">
+    <the-shop class="w-75"></the-shop>
+    <the-cart class="w-25"></the-cart>
+  </div>
+</template>
+
+<script>
+import TheShop from "./Shop/TheShop.vue";
+import TheCart from "./Cart/TheCart.vue";
+
+export default {
+  components: {
+    TheShop,
+    TheCart,
+  },
+};
+</script>
+
+<style></style>

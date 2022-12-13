57aec3370e55373213320eff57da57a21db0951a
cedMac <c.mace@webandcow.com>
2022-12-13 15:26:15 +0100

    mise en place du formulaire

--- src/components/features/Admin/AdminProductForm.vue
+++ src/components/features/Admin/AdminProductForm.vue
@@ -1,7 +1,42 @@
 <template>
-    <div>
-        Form
-    </div>
+  <div>
+    <form class="d-flex flex-column">
+        <h4>Ajouter un produit :</h4>
+      <div class="mb-3">
+        <label class="form-label">Image</label>
+        <input
+          type="text"
+          class="form-control"
+        />
+      </div>
+
+     <div class="mb-3">
+        <label class="form-label">Titre</label>
+        <input
+          type="text"
+          class="form-control"
+        />
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Description</label>
+        <textarea
+          type="textarea"
+          class="form-control"
+        ></textarea>
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Prix</label>
+        <input
+          type="number"
+          class="form-control"
+        />
+      </div>
+          
+      <button type="submit" class="btn btn-primary">Ajouter</button>
+    </form>
+  </div>
 </template>
 
 <script>
